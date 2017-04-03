fe16bf72dbe641bade01eba470176b062e567b87
JorgeeEnriquee <jorgee.enriqee@gmail.com>
2017-04-02 20:07:17 -0500

    Adding basic mapping

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"io"
 )
 
 func main(){
-	log.Println("Hello world")
+	http.HandleFunc("/",rootHandler)
+	http.HandleFunc("/init",initHandler)
+	http.HandleFunc("/init2",init2Handler)
+	http.ListenAndServe(":8080",nil)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request){
+	io.WriteString(w,"Hello world!")
+}
+
+func initHandler(w http.ResponseWriter, r *http.Request){
+	log.Println("Hey, this is the init function")
+}
+
+func init2Handler(w http.ResponseWriter, r *http.Request){
+	log.Println("Hey, this is the init2 functionn")
 }
